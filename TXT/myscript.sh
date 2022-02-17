30112254ec58bd234cfb2bf6bd135863438a50e2
Efrado Suryadi <58604575+efsrdi@users.noreply.github.com>
2022-02-17 20:58:00 +0700

    Completing Week 01 Assignment #1

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 # Mon 07 Feb 2022 23:02:21 WIB
 
-FILES="my*.txt my*.sh"
+WEEK="01"
+
+REC2="CE1CADB4A2B84EDB"
+REC1="63FB12B215403B20"
+FILES="my*.asc my*.txt my*.sh"
 SHA="SHA256SUM"
+RESDIR="$HOME/SP_RESULT/"
+
+[ -d $RESDIR ] || mkdir -p $RESDIR
+pushd $RESDIR
+for II in W?? ; do
+    [ -d $II ] || continue
+    TARFILE=my$II.tar.bz2
+    TARFASC=$TARFILE.asc
+    rm -vf $TARFILE $TARFASC
+    echo "tar cfj $TARFILE $II/"
+    tar cfj $TARFILE $II/
+    echo "gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE"
+    gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
+done
+popd
+
+if [[ "$WEEK" != "00" ]] ; then
+    II="${RESDIR}myW$WEEK.tar.bz2.asc"
+    echo "Check and move $II..."
+    [ -f $II ] && mv -vf $II .
+fi
 
 echo "rm -f $SHA $SHA.asc"
 rm -f $SHA $SHA.asc
